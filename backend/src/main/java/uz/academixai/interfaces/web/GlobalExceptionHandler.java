ce711cc2d0c0a5485c85a874a3eff01f17ff5048
muhammadjonsaidov <javascript0222@gmail.com>
2026-07-29 15:15:19 +0500

    space

--- backend/src/main/java/uz/academixai/interfaces/web/GlobalExceptionHandler.java
+++ backend/src/main/java/uz/academixai/interfaces/web/GlobalExceptionHandler.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
-
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
